90c802acf6dd5e1dab6515a9c9f528eeed48ea84
tajwarul23 <tajwarulchowdhury@gmail.com>
2026-05-25 10:05:01 +0600

    changes on puppetter

--- Backend/src/services/generatePDF.js
+++ Backend/src/services/generatePDF.js
@@ -1,33 +1,49 @@
 import puppeteer from "puppeteer";
 
 const generatePDF = async (html) => {
-  const browser = await puppeteer.launch({
-    headless: "new",
-    args: ["--no-sandbox", "--disable-setuid-sandbox","--disable-dev-shm-usage"],
-  });
+  try {
+    const browser = await puppeteer.launch({
+      headless: true,
+      ignoreDefaultArgs: ["--disable-extensions"],
+      args: [
+        "--no-sandbox",
+        "--use-gl=egl",
+        "--disable-setuid-sandbox",
+        "--disable-dev-shm-usage",
+      ],
+      ignoreHTTPSErrors: true,
+    });
 
-  const page = await browser.newPage();
+    const page = await browser.newPage();
+    await page.setUserAgent(
+      "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36",
+    );
+    await page.setContent(html, { waitUntil: "networkidle0" });
 
-  await page.setContent(html, { waitUntil: "networkidle0" });
+    //generate pdf
+    const pdfBuffer = await page.pdf({
+      format: "A4",
 
-  //generate pdf 
-  const pdfBuffer = await page.pdf({
-    format: "A4",
+      printBackground: true,
+    });
 
-    printBackground: true,
-  });
+    //generate thumbnail - screenshot of first page
+    await page.setViewport({ width: 794, height: 1123 });
+    const thumbnailBuffer = await page.screenshot({
+      type: "jpeg",
+      quality: 80,
+      clip: { x: 0, y: 0, width: 794, height: 1123 },
+    });
 
-  //generate thumbnail - screenshot of first page
-  await page.setViewport({ width: 794, height: 1123 });
-  const thumbnailBuffer = await page.screenshot({
-    type: "jpeg",
-    quality: 80,
-    clip: { x: 0, y: 0, width: 794, height: 1123 },
-  });
-
-  await browser.close();
-
-  return {pdfBuffer, thumbnailBuffer};
+    return { pdfBuffer, thumbnailBuffer };
+  } catch (error) {
+    console.log("Could not create a browser instance =>", error);
+    throw error;
+  } finally {
+    if (browser) {
+      await browser.close();
+    }
+  }
 };
 
 export default generatePDF;
