4e7dff2ad32d7cb455b9cb4afb13d63ad900cde3
tajwarul23 <tajwarulchowdhury@gmail.com>
2026-05-25 17:27:29 +0600

    change on transporter

--- Backend/src/services/transporter.js
+++ Backend/src/services/transporter.js
@@ -3,9 +3,12 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const transporter = nodemailer.createTransport({
-    service:"gmail",
-    auth:{
-        user:process.env.EMAIL_USER,
-        pass:process.env.EMAIL_PASS
-    }
-})
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+  service: "gmail",
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+});
