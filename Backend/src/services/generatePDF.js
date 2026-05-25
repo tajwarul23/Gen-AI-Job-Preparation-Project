b2bea1e6c7a22d485354b80a08cfb40f7efb3f08
tajwarul23 <tajwarulchowdhury@gmail.com>
2026-05-25 09:35:01 +0600

    change on package.json

--- Backend/src/services/generatePDF.js
+++ Backend/src/services/generatePDF.js
@@ -3,7 +3,7 @@ import puppeteer from "puppeteer";
 const generatePDF = async (html) => {
   const browser = await puppeteer.launch({
     headless: "new",
-    args: ["--no-sandbox", "--disable-setuid-sandbox"],
+    args: ["--no-sandbox", "--disable-setuid-sandbox","--disable-dev-shm-usage"],
   });
 
   const page = await browser.newPage();
