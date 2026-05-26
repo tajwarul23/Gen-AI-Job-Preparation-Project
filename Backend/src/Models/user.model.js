304372925cbcea51afd11b4535e28e971473d325
tajwarul23 <tajwarulchowdhury@gmail.com>
2026-05-26 20:54:25 +0600

    fix user model

--- Backend/src/Models/user.model.js
+++ Backend/src/Models/user.model.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const userSchema = new mongoose.Schema({
   userName: {
     type: String,
-    unique: [true, "username already taken"],
+    
     required: true,
   },
   email: {
