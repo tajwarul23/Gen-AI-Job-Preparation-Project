070cf10bdf587a2559441ec2a8affcb463ac91eb
tajwarul23 <tajwarulchowdhury@gmail.com>
2026-06-03 20:03:58 +0600

    fixed logout on loading on protected pages:

--- Frontend/src/features/Auth/Hooks/useAuth.js
+++ Frontend/src/features/Auth/Hooks/useAuth.js
@@ -85,5 +85,5 @@ export const useAuth = () => {
 
     getAndSetUser();
   }, []);
-  return { user, loading, handleRegister, handleLogin, handleLogout, error };
+  return { user, loading, handleRegister, handleLogin, handleLogout, error,isInitializing };
 };
