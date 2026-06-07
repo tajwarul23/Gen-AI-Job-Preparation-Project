af03908b7cb33f5ca65afb6555bfd521f8e586d1
tajwarul23 <tajwarulchowdhury@gmail.com>
2026-06-07 20:21:10 +0600

    resume provide file path edit

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -4,7 +4,6 @@ import "./index.css";
 import App from "./App.jsx";
 import { AuthProvider } from "./features/Auth/auth.context.jsx";
 import { InterviewProvider } from "./features/interview/interview.context.jsx";
-// import { ResumeProvider } from "./features/ResumeBuilder/Services/resume.context.jsx";
 import { ResumeProvider } from "./features/ResumeBuilder/Services/resume.context.jsx";
 
 createRoot(document.getElementById("root")).render(
